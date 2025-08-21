9320cacef3e140cf737a85c74565f1d614b41047
Leandro Amaro Garcia <leandroamarogarcia@Leandros-MacBook-Pro.local>
2025-08-21 19:02:48 -0300

    added logo

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
           <li>Contact</li>
         </ul>
         <p>{cartQty()}</p>
+        <p>Logo</p>
       </nav>
       <div>HOLA MUNDO</div>
       </header>
